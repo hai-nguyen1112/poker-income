208f4c839fe72e48b53902b1bf108e0c53e3f9b2
Hai Nguyen <hainguyen@RO-MAC-004-MBP.IDC-S2.Local>
2020-03-06 10:52:01 -0500

    fixex axios instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,13 +4,13 @@ let instance
 
 switch (window.location.hostname) {
     case "localhost":
-        instance = axios.create({baseURL: "http://localhost:4000"})
+        instance = axios.create({baseURL: "http://localhost:4000/"})
         break
     case "poker-income.netlify.com":
-        instance = axios.create({baseURL: "https://poker-income-backend.herokuapp.com"})
+        instance = axios.create({baseURL: "https://poker-income-backend.herokuapp.com/"})
         break
     default:
-        instance = axios.create({baseURL: "http://localhost:4000"})
+        instance = axios.create({baseURL: "http://localhost:4000/"})
 }
 
 export default instance
